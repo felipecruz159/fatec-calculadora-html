7c1d42a7fc1cefcfccd048b01292e6bf40ebb8c5
felipecruz159 <felipe.cruz159@hotmail.com>
2023-03-07 19:11:14 -0300

    funcionalidade limpar dados

--- custom.js
+++ custom.js
@@ -1,12 +1,36 @@
-function somar(){
-    var n1 = document.querySelector('#n1').value;
-    var n2 = document.querySelector('#n2').value;
+const somarInput = document.querySelector('#somar');
+const subInput   = document.querySelector('#subtrair');
+const multInput  = document.querySelector('#multiplicar');
+const divInput   = document.querySelector('#dividir');
 
-    var result = parseFloat(n1) + parseFloat(n2);
-    var input = document.querySelector('#result');
+const n1 = document.querySelector('#n1');
+const n2 = document.querySelector('#n2');
+const resultInput = document.querySelector('#result');
 
-    if (result){
-        input.value = result;
-    }
-    
-}
+const limpar = document.querySelector('#limpar');
+
+somarInput.addEventListener("click", function(){
+    let result = parseFloat(n1.value) + parseFloat(n2.value);
+    resultInput.value = result;
+})
+
+subInput.addEventListener("click", function(){
+    let result = parseFloat(n1.value) - parseFloat(n2.value);
+    resultInput.value = result;
+})
+
+multInput.addEventListener("click", function(){
+    let result = parseFloat(n1.value) * parseFloat(n2.value);
+    resultInput.value = result;
+})
+
+divInput.addEventListener("click", function(){
+    let result = parseFloat(n1.value) / parseFloat(n2.value);
+    resultInput.value = result;
+})
+
+limpar.addEventListener("click", function(){
+    n1.value = '';
+    n2.value = '';
+    resultInput.value = '';
+})
